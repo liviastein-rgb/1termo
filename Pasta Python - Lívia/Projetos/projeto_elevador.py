255d2887d3665161a2ae803b0845345767c24182
Lívia Stein <liviasteinpereira3@gmail.com>
2026-05-08 11:22:13 -0300

    Atuaçlização projeto elevador

---
+++ Pasta Python - Lívia/Projetos/projeto_elevador.py
@@ -0,0 +1,14 @@
+#Sistema de Elevador de Prédio
+
+#O predio possui 10 andares, sendo térreo o andar 0. O elevador pode se mover para cima e para baixo, e tem a capacidade de transportar até 5 pessoas.
+#  O elevador começa no andar 0 e pode ser chamado por qualquer pessoa em qualquer andar.
+#  O elevador deve se mover para o andar onde a pessoa chamou, e depois para o andar destino da pessoa
+#O elevador deve exibir mensagens indicando o andar atual, o número de pessoas no elevador, e as ações realizadas (subindo, descendo, parando).
+#  O programa deve continuar rodando até que o usuário decida encerrar
+
+# Se o elevador está subindo, ele deve atender todas as chamadas de "subida" acima dele antes de inverter a direção para atender quem quer descer.
+#Se passageiros == 5, o elevador deve ignorar novas chamadas de andares 
+# 
+# Abrir ao chegar no andar.
+# Botão de "Abrir Porta" e "Fechar Porta"
+# Se algo bloquear a porta, o motor de fechamento deve reverter imediatamente. 
